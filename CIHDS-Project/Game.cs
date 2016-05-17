997f726b45b724386854a671c933ec2e057f73b6
okuiri <adammoham@gmail.com>
2016-05-17 13:14:45 -0400

    Walking Stages implemented.

--- CIHDS-Project/Game.cs
+++ CIHDS-Project/Game.cs
@@ -11,20 +11,37 @@ namespace CIHDS_Project
     {
 
 
-        public static string StatusText = "Raise both your hands above your shoulders!";
-        public enum GameState { Begin, Calibrating, Stage2, Stage3, Stage4, Finish};
+        public static string StatusText = "Raise both your hands above your shoulders to play!";
+        public enum GameState { Begin, Calibrating, FwdWalk, BwdWalk, SaveData, Finish};
         public static GameState gameState = GameState.Begin;
 
         private static int count = 0;
-        private static float upperPositionThreshold = 2.5f;
+        //Start Position for fwd walking stage
+        //Ending Position for backward walking stage
         private static float lowerPositionThreshold = 2.3f;
+        private static float upperPositionThreshold = 2.5f;
+
+
+        // Finish position for walking forward
+        // Start Position for walking backward
+        private static float lowerWalkingThreshold = 0.9f;
+        private static float upperWalkingThreshold = 1.3f;
 
         public static void RunGame(this Body b)
         {
             var joints = b.Joints;
+            
+            CameraSpacePoint shoulder = joints[JointType.ShoulderLeft].Position;
+
+            if (shoulder.Z < 0)
+            {
+                shoulder.Z = 0.1f;
+            }
+
             switch (gameState)
             {
                 case GameState.Begin:
+                    StatusText = "Raise both your hands above your shoulders to play!";
                     // Check both hands above shoulders
                     float rhand = joints[JointType.HandRight].Position.Y;
                     float lhand = joints[JointType.HandLeft].Position.Y;
@@ -45,15 +62,7 @@ namespace CIHDS_Project
                     
                     break;
                 case GameState.Calibrating:
-                    StatusText = "Move to 4 Feet away";
-                    // Check the top of spine for distance from kinect
-                    CameraSpacePoint shoulder = joints[JointType.ShoulderLeft].Position;
-                    
-                    // Clamping
-                    if (shoulder.Z < 0)
-                    {
-                        shoulder.Z = 0.1f;
-                    }
+                    StatusText = "Move into position!";
 
                     if(shoulder.Z > upperPositionThreshold)
                     {
@@ -67,21 +76,60 @@ namespace CIHDS_Project
                     }
                     else
                     {
-                        StatusText += " Right There!";
+                        StatusText = "Stop Right There!";
                         count++;
                         if (count > 50)
                         {
                             count = 0;
-                            gameState = GameState.Stage2;
+                            gameState = GameState.FwdWalk;
                         }
                     }
                     break;
-                case GameState.Stage2:
-                    StatusText = "Now on stage 2";
+                case GameState.FwdWalk:
+                    StatusText = "Move forward slowly";
+                    
+                    if(shoulder.Z < upperWalkingThreshold && shoulder.Z > lowerWalkingThreshold)
+                    {
+                        StatusText = "Stop!";
+                        if(shoulder.Z < upperPositionThreshold)
+                        count++;
+                        if (count > 5)
+                        {
+                            count = 0;
+                            gameState = GameState.BwdWalk;
+                        }
+                    }
                     break;
-                case GameState.Stage3:
+                case GameState.BwdWalk:
+                    StatusText = "Walk back to the start";
+                    if(shoulder.Z > upperPositionThreshold)
+                    {
+                        StatusText = StatusText + " - move forward";
+                        count = 0;
+                    }
+                    else if (shoulder.Z < lowerPositionThreshold)
+                    {
+                        StatusText = StatusText + " - move backward";
+                        count = 0;
+                    }
+                    else
+                    {
+                        StatusText = "Stop Right There!";
+                        count++;
+                        if (count > 30)
+                        {
+                            count = 0;
+                            gameState = GameState.SaveData;
+                        }
+                    }
+
                     break;
-                case GameState.Stage4:
+                case GameState.SaveData:
+                    StatusText = "Exercises Finished!"; 
+                    //Save Dialog 
+                    break;
+                case GameState.Finish:
+                    StatusText = "Thank You for Playing!";
                     break;
             }
         }
